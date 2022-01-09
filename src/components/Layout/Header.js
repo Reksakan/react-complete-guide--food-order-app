aae3377a11d365df72bac44c89f7316d7b4ae1e1
unknown <aalshanbayev@gmail.com>
2022-01-09 02:11:57 -0500

    Modal pop-up window and some presentational components are realised

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
-import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
 
 const Header = props => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton onClick={props.onShowCart}/> 
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
