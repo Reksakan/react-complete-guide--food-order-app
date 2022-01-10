c232c50c24243ee64dc429c10c3e38569117e9f1
unknown <aalshanbayev@gmail.com>
2022-01-10 01:45:40 -0500

    useReducer for realizing new card adding functionality

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,16 +8,30 @@ const defaultCartState = {
 }
 
 const cartReducer = (state, action) => {
+  if(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
   return defaultCartState;
 };
 
 const CartProvider = props => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartState] = useReducer(cartReducer, defaultCartState)
+  
+  const addItemToCartHandler = (item) => {
+    dispatchCartState({type: 'ADD', item: item})
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartState({type: 'REMOVE', id: id})
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   }
